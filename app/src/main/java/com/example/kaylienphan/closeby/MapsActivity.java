6ecdfeb9dd12cf659901150a35d193c40f6e171a
Kaylien Phan <kaylienphan@gmail.com>
2018-12-20 18:37:37 -0500

    adding api.xml to .gitignore && app now enables location tracking

--- app/src/main/java/com/example/kaylienphan/closeby/MapsActivity.java
+++ app/src/main/java/com/example/kaylienphan/closeby/MapsActivity.java
@@ -1,16 +1,25 @@
 package com.example.kaylienphan.closeby;
 
+import android.content.IntentSender;
 import android.support.v4.app.FragmentActivity;
 import android.os.Bundle;
+import android.util.Log;
 
+import com.google.android.gms.common.api.ApiException;
+import com.google.android.gms.common.api.ResolvableApiException;
 import com.google.android.gms.location.LocationRequest;
+import com.google.android.gms.location.LocationServices;
 import com.google.android.gms.location.LocationSettingsRequest;
+import com.google.android.gms.location.LocationSettingsResponse;
+import com.google.android.gms.location.LocationSettingsStatusCodes;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
 
 public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
 
@@ -31,7 +40,65 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
 
         LocationSettingsRequest.Builder builder = new LocationSettingsRequest.Builder()
                 .addLocationRequest(mLocationRequestHighAccuracy);
-        //.addLocationRequest(mLocationRequestBalancedPowerAccuracy);
+
+        Task<LocationSettingsResponse> result =
+                LocationServices.getSettingsClient(this).checkLocationSettings(builder.build());
+
+        //TODO: When you initially give permission to enable location tracking,
+        // TODO: it doesn't continuously give the location because this addOnCompleteListener is in onCreate
+
+        result.addOnCompleteListener(new OnCompleteListener<LocationSettingsResponse>() {
+            @Override
+            public void onComplete(Task<LocationSettingsResponse> task) {
+                try {
+                    LocationSettingsResponse response = task.getResult(ApiException.class);
+                    // All location settings are satisfied. The client can initialize location
+                    // requests here.
+                    Log.d("response var", response.toString());
+
+                    Log.d("we in this one", "5");
+                } catch (ApiException exception) {
+                    switch (exception.getStatusCode()) {
+                        case LocationSettingsStatusCodes.RESOLUTION_REQUIRED:
+                            // Location settings are not satisfied. But could be fixed by showing the
+                            // user a dialog.
+                            try {
+                                // Cast to a resolvable exception.
+                                ResolvableApiException resolvable = (ResolvableApiException) exception;
+                                // Show the dialog by calling startResolutionForResult(),
+                                // and check the result in onActivityResult().
+                                resolvable.startResolutionForResult(
+                                        MapsActivity.this, 1);
+                                Log.d("we in this one", "1");
+                            } catch (IntentSender.SendIntentException e) {
+                                // Ignore the error.
+                                Log.d("we in this one", "2");
+                            } catch (ClassCastException e) {
+                                // Ignore, should be an impossible error.
+
+                                Log.d("we in this one", "3");
+                            }
+                            break;
+                        case LocationSettingsStatusCodes.SETTINGS_CHANGE_UNAVAILABLE:
+                            // Location settings are not satisfied. However, we have no way to fix the
+                            // settings so we won't show the dialog.
+                            Log.d("addOnCompleteListener", "SETTINGS CHANGE UNAVAILABLE");
+
+                            Log.d("we in this one", "4");
+                            break;
+                    }
+                }
+            }
+        });
+//        LocationSettingsResponse response = result.getResult();
+//
+//        try {
+//            Log.d("Is location usable", response.getLocationSettingsStates().isLocationUsable()+"");
+//        } catch (NullPointerException n) {
+//            Log.d("Is location usable", "Null pointer, no it isn't");
+//        }
+
+        //Log.d("LocationSettingsSuccess", result.isSuccessful()+"");
     }
 
 
