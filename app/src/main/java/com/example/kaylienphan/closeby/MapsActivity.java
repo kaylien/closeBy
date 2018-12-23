132d81f84b27522a3eb36025e391c698f549addb
Kaylien Phan <kaylienphan@gmail.com>
2018-12-22 21:02:46 -0500

    returns correct location of user

--- app/src/main/java/com/example/kaylienphan/closeby/MapsActivity.java
+++ app/src/main/java/com/example/kaylienphan/closeby/MapsActivity.java
@@ -8,6 +8,8 @@ import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.FragmentActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
 import com.google.android.gms.common.api.ApiException;
@@ -26,11 +28,13 @@ import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
 import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.android.gms.tasks.Task;
 
 import java.util.Locale;
 
-public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
+// TODO: Decide if you want to remove the OnClickListener implementation later?
+public class MapsActivity extends FragmentActivity implements OnMapReadyCallback, View.OnClickListener {
 
     private GoogleMap mMap;
 
@@ -53,13 +57,17 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_maps);
+
+        // Just for tutorial on getting current location
+        // https://androidclarified.com/fusedlocationproviderclient-current-location-example/
+        Button button = findViewById(R.id.button);
+        button.setOnClickListener(this);
+
         // Obtain the SupportMapFragment and get notified when the map is ready to be used.
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
                 .findFragmentById(R.id.map);
         mapFragment.getMapAsync(this);
 
-        // Will be used to retrieve the last known location of the user
-        mFusedLocationClient = LocationServices.getFusedLocationProviderClient(this);
 
         //TODO: Decide if you really want to set the Location Request right here. Will it run onPause()?
         LocationRequest mLocationRequestHighAccuracy = new LocationRequest();
@@ -121,6 +129,50 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         });
     }
 
+    @Override
+    public void onClick(View view) {
+        Log.d("OnClick reached", "yeah");
+        switch (view.getId()) {
+            case R.id.button:
+                if (ActivityCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+
+                    Log.d("OnClick reached", "but permissions failed");
+                    Log.d("package manager code", PackageManager.PERMISSION_GRANTED+ "");
+                    Log.d("fine_location", ActivityCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_FINE_LOCATION)+"");
+                    Log.d("coarse_location", ActivityCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_COARSE_LOCATION)+"");
+
+                    // Asks the user to enable location
+                    ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, 200);
+                    return;
+                }
+
+                // Will be used to retrieve the last known location of the user
+                mFusedLocationClient = LocationServices.getFusedLocationProviderClient(this);
+                Task task = mFusedLocationClient.getLastLocation();
+                task.addOnSuccessListener(this, new OnSuccessListener<Location>() {
+                    @Override
+                    public void onSuccess(Location location) {
+                        if(location!=null) {
+                            // Write your implemenation here
+                            Log.d("AndroidClarified",location.getLatitude()+" "+location.getLongitude()); }
+                    }
+                });
+
+                //Log.d("Idk", "something's not right");
+                break;
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, String[] permissions, int[] grantResults) {
+        switch (requestCode) {
+            case 200: {
+                if(grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    // {Some Code}
+                }
+            }
+        }
+    }
 
     /**
      * Manipulates the map once available.
